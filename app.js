8933a946aa6354a6caebed221e3fe52f12691955
y5boss <77240689+y5boss@users.noreply.github.com>
2021-01-11 12:40:33 +0000

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const lines = document.querySelector('.lines');
-const menu = document.querySelector('nav ul');
-
-lines.addEventListener('click', toggleMenu);
-
-function toggleMenu(){
-    menu.classList.toggle('active');
-}
